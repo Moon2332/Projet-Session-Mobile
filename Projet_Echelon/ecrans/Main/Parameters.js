1a7442685df0f74fd010db8f019e067d741ed4f5
Moon2332 <tonymoundoubou@gmail.com>
2025-03-03 14:57:31 -0500

    Update

---
+++ Projet_Echelon/ecrans/Main/Parameters.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Parameters = () => {
+  return (
+    <View>
+      <Text>Parameters</Text>
+    </View>
+  )
+}
+
+export default Parameters
+
+const styles = StyleSheet.create({})
